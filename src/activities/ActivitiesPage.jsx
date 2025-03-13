bd5b8ebb07e5ad351c058b55d555169a7ff7bb15
Lester Lee <contact@lester-lee.com>
2025-03-13 16:35:53 -0400

    Write solution

--- src/activities/ActivitiesPage.jsx
+++ src/activities/ActivitiesPage.jsx
@@ -1,8 +1,20 @@
+import { useAuth } from "../auth/AuthContext";
+
+import ActivityList from "./ActivityList";
+import ActivityForm from "./ActivityForm";
+
+/**
+ * All users can see a list of activities.
+ * If they are logged in, they will also see a form to create an activity.
+ */
 export default function ActivitiesPage() {
+  const { token } = useAuth();
+
   return (
     <>
       <h1>Activities</h1>
-      <p>Imagine all the activities!</p>
+      <ActivityList />
+      {token && <ActivityForm />}
     </>
   );
 }
